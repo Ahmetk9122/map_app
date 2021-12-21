1c78622356053fbcccbd6fb8f2c854dae472d0f6
Ahmet Karabudak <ahmet.karabudakk.9122@gmail.com>
2021-12-21 16:40:06 +0300

    Map_app

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Sabitler{
+  static final TextStyle baslikStyle =GoogleFonts.quicksand(
+    fontWeight: FontWeight.w900
+  );
+  static final TextStyle CardStyle =GoogleFonts.quicksand(
+    fontSize:16 ,color:Colors.black
+  );
+}
